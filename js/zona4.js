ae262658a4bf26957dbe817d1235bd1856b579c0
Jesica56 <martinezjesicanoelia56@gmail.com>
2023-12-05 22:16:28 -0300

    cambios en los anchos del mapa

--- js/zona4.js
+++ js/zona4.js
@@ -1,26 +1,56 @@
 
+// Crear un mapa centrado en Salta
+const mapa = new ol.Map({
+    target: 'mapa',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([-65.4117, -24.7859]),
+        zoom: 10
+    })
+});
 
-// Coordenadas de las zonas
+// Definir las zonas y sus coordenadas
 const zonas = [
-    { nombre: "Guachipas", coordenadas: [-25.121, -65.564] }, 
-    { nombre: "Rosario del Lerma", coordenadas: [-24.921, -65.521] },
-    { nombre: "Campo Quijano", coordenadas: [-24.889, -65.758] },
-    { nombre: "El Encon", coordenadas: [-24.884, -65.541] },
-    { nombre: "Cerrillos", coordenadas: [-24.896, -65.467] }
+    { nombre: "Guachipas", coordenadas: ol.proj.fromLonLat([-25.52216765,-65.5083359012118]) },
+    { nombre: "Rosario del Lerma", coordenadas: ol.proj.fromLonLat([-24.53873, -65.8991090851796]) },
+    { nombre: "Campo Quijano", coordenadas: ol.proj.fromLonLat([-24.9079731, -65.6425972]) },
+    { nombre: "El Encon", coordenadas: ol.proj.fromLonLat([-24.8515869, -65.5542379]) },
+    { nombre: "Cerrillos", coordenadas: ol.proj.fromLonLat([-24.8970543, -65.484497]) }
 ];
 
-// Crear el mapa centrado en Salta
-const mapa = L.map('mapa').setView([-24.7859, -65.4117], 14);
+// Crear una capa vectorial para los marcadores
+const vectorSource = new ol.source.Vector();
+const vectorLayer = new ol.layer.Vector({
+    source: vectorSource
+});
 
-// Añadir un "tile layer" (capa de mapa) de OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(mapa);
+mapa.addLayer(vectorLayer);
 
-// Añadir marcadores para cada zona
+// Añadir un marcador para cada zona
 zonas.forEach(zona => {
-    const marcador = L.marker(zona.coordenadas).addTo(mapa);
-    
-    // Añadir un popup al marcador con el nombre de la zona
-    marcador.bindPopup(zona.nombre);
+    const marcador = new ol.Feature({
+        geometry: new ol.geom.Point(zona.coordenadas),
+        name: zona.nombre
+    });
+
+    // Estilo del marcador
+    marcador.setStyle(new ol.style.Style({
+        image: new ol.style.Circle({
+            radius: 15,
+            fill: new ol.style.Fill({ color: 'red' }),
+            stroke: new ol.style.Stroke({ color: 'white', width: 4 })
+        })
+    }));
+
+    vectorSource.addFeature(marcador);
+
+    // Añadir un popup al hacer clic en el marcador
+    marcador.on('click', function (event) {
+        const nombre = event.target.values_.name;
+        alert("Zona: " + nombre);
+    });
 });
